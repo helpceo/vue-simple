14c6c1afe44ac76340f6149a91eaa5f2eb72f179
helpceo <helpceo@126.com>
2018-11-27 13:11:17 +0800

    first commit

---
+++ src/components/Home/index.vue
@@ -0,0 +1,10 @@
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+  },
+};
+</script>
+<template src="./index.html"></template>
+<style scoped lang="scss" src="./index.scss"></style>
